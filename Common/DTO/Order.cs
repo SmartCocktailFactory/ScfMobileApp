a3d2c93525427556f649878a9c20f64969731b7a
icebear8 <ebert.dominik@gmail.com>
2013-11-17 12:25:25 +0100

    Order ctor added

--- Common/DTO/Order.cs
+++ Common/DTO/Order.cs
@@ -5,10 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Common.DTO {
+
   class Order {
     public string OrderId { get; set; }
     public string DrinkId { get; set; }
     public string OrderStatus { get; set; }
     public int ExpectedSecondsToDeliver { get; set; }
+
+    public Order() {
+      this.OrderId = string.Empty;
+      this.DrinkId = string.Empty;
+      this.OrderStatus = string.Empty;
+      this.ExpectedSecondsToDeliver = 60;
+    }
   }
 }
