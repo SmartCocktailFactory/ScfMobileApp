123731ce6c6131f7c607a4b36968604fb6fe21a2
icebear8 <ebert.dominik@gmail.com>
2013-11-17 13:45:37 +0100

    Added WPF gui client

---
+++ ScfApp.SimGui/SignInView.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScfApp.SimGui {
+  /// <summary>
+  /// Interaction logic for SignInView.xaml
+  /// </summary>
+  public partial class SignInView : UserControl {
+    #region Members
+    public Common.ViewModel.SignInViewModel _mySignInViewModel = new Common.ViewModel.SignInViewModel();
+    #endregion
+
+    public SignInView() {
+      InitializeComponent();
+    }
+
+    #region GUI event handler
+    private void UserControl_Loaded(object sender, RoutedEventArgs e) {
+      this.DataContext = this._mySignInViewModel;
+      this.tbxWelcomeMessage.Text = "none";
+      this._mySignInViewModel.RemoteUrl = "http://192.168.1.40:12345";
+      this._mySignInViewModel.OnViewModelChanged += _mySignInViewModel_OnViewModelChanged;
+      this.tbxWelcomeMessage.Text = this._mySignInViewModel.WelcomeMessage;
+    }
+    #endregion
+
+    #region Model event handler
+    void _mySignInViewModel_OnViewModelChanged(object sender, Common.ViewModel.ViewModelChangedEventArgs e) {
+      this.Dispatcher.Invoke(delegate {
+        this.tbxWelcomeMessage.Text = this._mySignInViewModel.WelcomeMessage;
+      });
+    }
+    #endregion
+  }
+}
