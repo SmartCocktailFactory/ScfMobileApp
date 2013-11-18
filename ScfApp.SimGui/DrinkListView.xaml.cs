e5addc985c9f59331d1fb480ad3720ae1818e338
icebear8 <ebert.dominik@gmail.com>
2013-11-18 18:50:25 +0100

    Place orders possible

--- ScfApp.SimGui/DrinkListView.xaml.cs
+++ ScfApp.SimGui/DrinkListView.xaml.cs
@@ -19,24 +19,25 @@ namespace ScfApp.SimGui {
   /// </summary>
   public partial class DrinkListView : UserControl {
     #region Members
-    private Common.ViewModel.DrinkViewModel _Model = new Common.ViewModel.DrinkViewModel();
+    private Common.ViewModel.DrinkViewModel _DrinkModel = new Common.ViewModel.DrinkViewModel();
+    private Common.ViewModel.OrderViewModel _OrderModel = new Common.ViewModel.OrderViewModel();
     #endregion
 
     public DrinkListView() {
       InitializeComponent();
-      this._Model.OnViewModelChanged += _Model_OnViewModelChanged;
+      this._DrinkModel.OnViewModelChanged += _Model_OnViewModelChanged;
     }
 
     #region Public methods
     public void Reload() {
-      this._UpdateDrinks(this._Model.Drinks);
+      this._UpdateDrinks(this._DrinkModel.Drinks);
     }
     #endregion
 
     #region Model event handler
     void _Model_OnViewModelChanged(object sender, Common.ViewModel.ViewModelChangedEventArgs e) {
       this.Dispatcher.Invoke(delegate {
-        this._UpdateDrinks(this._Model.Drinks);
+        this._UpdateDrinks(this._DrinkModel.Drinks);
       });
     }
     #endregion
@@ -50,5 +51,12 @@ namespace ScfApp.SimGui {
     }
     #endregion
 
+    private void btnOrder_Click(object sender, RoutedEventArgs e) {
+      int selectedIndex = this.lstView.SelectedIndex;
+      if (selectedIndex < 0) {
+        return;
+      }
+      this._OrderModel.OrderDrink(this._DrinkModel.Drinks[selectedIndex].DrinkId);
+    }
   }
 }
